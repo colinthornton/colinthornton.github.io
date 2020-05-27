3fda464d8ec0f7dabbb56bed6153f6aa5e788ce2
Colin Thornton <colinthorntontx@gmail.com>
2020-05-27 15:14:52 +0900

    change index title

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const Index = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="colinthornton" />
+      <SEO title="/" />
       <nav>
         <ul
           style={{
